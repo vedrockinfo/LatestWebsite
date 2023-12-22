5c471eef3fa37c25406bf6dac5be05b9ae6c9a47
VedantCMS <vedantgupta.cms@gmail.com>
2023-12-22 16:25:23 +0530

    first global commit

---
+++ js/main.js
@@ -0,0 +1,221 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  window.addEventListener('scroll', function () {
+
+    if (window.scrollY > 300) {
+      document.getElementById('navbar_top').classList.add('fixed-top');
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+    } else {
+      document.getElementById('navbar_top').classList.remove('fixed-top');
+      // remove padding top from body
+      document.body.style.paddingTop = '0';
+    }
+  });
+});
+
+jQuery(function () {
+  jQuery("#header").load("include/header.html");
+  jQuery("#footer").load("include/footer.html");
+});
+
+// Loader while loading
+
+window.addEventListener('load', function () {
+  document.getElementById('loader').style.display = 'none';
+});
+
+$('.hero-slider').owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: true,
+  touchDrag: false,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  mouseDrag: false,
+  animateOut: 'fadeOut',
+  navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+})
+
+$('.inspires-life').owlCarousel({
+  loop: true,
+  margin: 40,
+  nav: true,
+  navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+  dots: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 2
+    }
+  }
+})
+
+$('.testimonial-carousel').owlCarousel({
+  loop: true,
+  margin: 40,
+  nav: true,
+  navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+  dots: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 2
+    }
+  }
+})
+
+const html = document.querySelector('html');
+html.setAttribute('data-bs-theme', 'dark');
+
+document.addEventListener('DOMContentLoaded', () => {
+  // --- Create LightBox
+  const galleryGrid = document.querySelector(".gallery-grid");
+  const links = galleryGrid.querySelectorAll("a");
+  const imgs = galleryGrid.querySelectorAll("img");
+  const lightboxModal = document.getElementById("lightbox-modal");
+  const bsModal = new bootstrap.Modal(lightboxModal);
+  const modalBody = lightboxModal.querySelector(".lightbox-content");
+
+  function createCaption(caption) {
+    return `<div class="carousel-caption d-none d-md-block">
+        <h4 class="m-0">${caption}</h4>
+      </div>`;
+  }
+
+  function createIndicators(img) {
+    let markup = "", i, len;
+
+    const countSlides = links.length;
+    const parentCol = img.closest('.col');
+    const curIndex = [...parentCol.parentElement.children].indexOf(parentCol);
+
+    for (i = 0, len = countSlides; i < len; i++) {
+      markup += `
+        <button type="button" data-bs-target="#lightboxCarousel"
+          data-bs-slide-to="${i}"
+          ${i === curIndex ? 'class="active" aria-current="true"' : ''}
+          aria-label="Slide ${i + 1}">
+        </button>`;
+    }
+
+    return markup;
+  }
+
+  function createSlides(img) {
+    let markup = "";
+    const currentImgSrc = img.closest('.gallery-item').getAttribute("href");
+
+    for (const img of imgs) {
+      const imgSrc = img.closest('.gallery-item').getAttribute("href");
+      const imgAlt = img.getAttribute("alt");
+
+      markup += `
+        <div class="carousel-item${currentImgSrc === imgSrc ? " active" : ""}">
+          <img class="d-block img-fluid w-100" src=${imgSrc} alt="${imgAlt}">
+          ${imgAlt ? createCaption(imgAlt) : ""}
+        </div>`;
+    }
+
+    return markup;
+  }
+
+  function createCarousel(img) {
+    const markup = `
+      <!-- Lightbox Carousel -->
+      <div id="lightboxCarousel" class="carousel slide carousel-fade" data-bs-ride="true">
+        <!-- Indicators/dots -->
+        <div class="carousel-indicators">
+          ${createIndicators(img)}
+        </div>
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner justify-content-center mx-auto">
+          ${createSlides(img)}
+        </div>
+        <!-- Controls/icons -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#lightboxCarousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#lightboxCarousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      `;
+
+    modalBody.innerHTML = markup;
+  }
+
+  for (const link of links) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const currentImg = link.querySelector("img");
+      const lightboxCarousel = document.getElementById("lightboxCarousel");
+
+      if (lightboxCarousel) {
+        const parentCol = link.closest('.col');
+        const index = [...parentCol.parentElement.children].indexOf(parentCol);
+
+        const bsCarousel = new bootstrap.Carousel(lightboxCarousel);
+        bsCarousel.to(index);
+      } else {
+        createCarousel(currentImg);
+      }
+
+      bsModal.show();
+    });
+  }
+
+  // --- Support Fullscreen
+  const fsEnlarge = document.querySelector(".btn-fullscreen-enlarge");
+  const fsExit = document.querySelector(".btn-fullscreen-exit");
+
+  function enterFS() {
+    lightboxModal.requestFullscreen().then({}).catch(err => {
+      alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+    });
+    fsEnlarge.classList.toggle("d-none");
+    fsExit.classList.toggle("d-none");
+  }
+
+  function exitFS() {
+    document.exitFullscreen();
+    fsExit.classList.toggle("d-none");
+    fsEnlarge.classList.toggle("d-none");
+  }
+
+  fsEnlarge.addEventListener("click", (e) => {
+    e.preventDefault();
+    enterFS();
+  });
+
+  fsExit.addEventListener("click", (e) => {
+    e.preventDefault();
+    exitFS();
+  });
+})
