0fe977c5d40eae6d7a597990865d93ca18a2af82
VedantCMS <vedantgupta.cms@gmail.com>
2024-01-09 12:53:28 +0530

    pushed buy verdant

--- js/main.js
+++ js/main.js
@@ -79,3 +79,24 @@ $('.awards').owlCarousel({
     }
   }
 })
+
+$('.com-future').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  smartSpeed: 800,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+})
